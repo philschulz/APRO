8c37a8765e03056612c63d0fc81d7ad8bf9055f6
philschulz <P.schulz@uva.nl>
2016-06-16 13:56:51 +0200

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,8 +1,12 @@
 import sys
 from Moses_Wrapper import Moses_Wrapper
 
-fields = sys.argv[1:]
-print fields
-wrapper = Moses_Wrapper(fields[0])
+def main():
+    fields = sys.argv[1:]
+    print fields
+    wrapper = Moses_Wrapper(fields[0])
 
-wrapper.merge_k_best_lists(fields[1], fields[2])
+    wrapper.merge_k_best_lists(fields[1], fields[2])
+
+if __name__ == "__main__":
+    main()
